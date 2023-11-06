1dd5a2973088174776c88e3788ec10beeb9f490b
Meet Goti <meetgoti07@gmail.com>
2023-11-06 12:01:31 +0530

    Analytics

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from "react-dom/client";
 import App from './app/App';
 import './index.css';
+import ReactGA from 'react-ga';
+ReactGA.initialize('G-4N9KRFL7N7'); // add your tracking id here.
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
